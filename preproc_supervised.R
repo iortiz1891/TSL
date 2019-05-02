ce62f94dda34f9aeba19d512e0ff57a924279105
Andrea Somazzi <andrea.somazzi@sns.it>
2019-05-02 16:36:44 +0200

    Code to obtain clean export data

--- preproc_supervised.R
+++ preproc_supervised.R
@@ -5,7 +5,7 @@ library(dplyr)
 #-------------------
 
 # Selection varibles
-years = seq(from = 2000, to = 2000)
+years = seq(from = 1995, to = 2017)
 country = "Italy"
 
 # Extract raw data from UN Comtrade API
@@ -19,6 +19,22 @@ for (y in years){
   
   # Drop the columns of the dataframe
    data_year <- select(data_year, c(year, commodity_code, trade_value_usd))
-   dummy <- melt.data.frame(data_year)
-   dummy <- cast(dummy, variable ~ commodity_code)
+   
+  # At the end of the "for" cycle, "df" will be the same of "data_year", with the difference that
+  # it will include all the years that we want
+   if (y==min(years)){
+     df <- data_year
+   }
+   if(y>min(years)){
+     df <- full_join(df,data_year) # This attaches the new "data_year" to the existing "df"
+   }
 }
+
+# We obtain the dataset as we want it by casting df.
+# Notice that if a good [column], has no value for some year [row], the corresponing cell is NA
+data_alltimes <- cast(df, year ~ commodity_code)
+
+# Labeling row names with the corresponding year and removing the first column
+row.names(data_alltimes)<- data_alltimes$year
+data_alltimes <- data_alltimes[,2:length(data_alltimes)]
+
