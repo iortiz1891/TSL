f9276bd119a7ea5c1cd59f443f51f5e1cf55da51
LeonardoIalongo <leonardo.ialongo@gmail.com>
2019-05-04 11:19:48 +0200

    Fixed empty rows problem

--- preproc_gdp.R
+++ preproc_gdp.R
@@ -4,7 +4,7 @@ library(dplyr)
 #-------------------
 
 # Import from csv
-gdp <- read.csv("Data/gdp_data.csv", na.strings = "..")
+gdp <- read.csv("Data/gdp_data.csv", na.strings = c("", ".."))
 names(gdp) <- c("Series Name", "Series Code", "Country Name", "Country", 1960:2018)
 gdp <- select(gdp, -c("Series Name", "Series Code", "Country Name"))
-
+gdp <- gdp[!(is.na(gdp$Country)),]
