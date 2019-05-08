fd68ae1e2e49ebe04b990e25a8e1feee0e186597
Andrea Somazzi <andrea.somazzi@sns.it>
2019-05-08 17:57:22 +0200

    Some improvement on lasso, still many things to fix

--- lasso.R
+++ lasso.R
@@ -38,8 +38,36 @@ ytrain <- data.matrix(ytrain)
 #Setting NA in predictors at 0 (NA means that that good as not been exported that year by that country)
 xtrain[is.na(xtrain)] <- 0
 
-grid=10^seq(10,-2,length=100)
-lasso.mod=glmnet(xtrain,ytrain,alpha=1,lambda=grid)
-plot(lasso.mod)
+#Scaling of xtrain in the naive way: centering columns at zero and dividing them by stddev
+xtrain_scaled <- scale(xtrain, center = TRUE, scale = TRUE)
+
+
+#!!!!!--FROM NOW LASSO--!!!!!
+
+
+#Dividing our train dataset in "train" and "test", at random. We need this to pick lambda.
+#Later, we will use this whole dataset as training set, and the "test" ones will be the future years
+set.seed(10)
+train=sample(1:nrow(xtrain_scaled), nrow(xtrain_scaled)/2) #Dividing dataset in two subsets of equal number of observations, at random
+test=(-train)
+
+grid=10^seq(10,-2,length=100) #Grid of lambdas
+lasso.mod=glmnet(xtrain_scaled[train,],ytrain[train,],alpha=1,lambda=grid) #Estimating betas from the train subset, at varius lambdas
+#plot(lasso.mod)
+
+cv.out <- cv.glmnet(xtrain_scaled[train,],ytrain[train,],alpha=1) #Do CV on the train subset
+#plot(cv.out)
+bestlam=cv.out$lambda.min
+
+lasso.pred <- predict(lasso.mod,s=bestlam,newx=xtrain_scaled[test,]) #Predicts on the test subset
+#mean((lasso.pred-ytrain[test,])^2)
+
+#Not so clear here
+out=glmnet(xtrain_scaled,ytrain,alpha=1,lambda=grid)
+lasso.coef=predict(out,type="coefficients",s=bestlam)[1:ncol(xtrain_scaled),]
+lasso.coef
+lasso.coef[lasso.coef!=0]
+
+
 
 
